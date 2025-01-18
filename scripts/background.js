868969e14cf91855862c433050abe35cd3f8220a
jianbing <yeahitsyjb@gmail.com>
2025-01-18 21:15:25 +0800

    changed the css changed option

--- scripts/background.js
+++ scripts/background.js
@@ -5,11 +5,11 @@ chrome.runtime.onInstalled.addListener(() => {
     });
   });
   
-  const extensions = 'https://developer.chrome.com/docs/extensions';
-  const webstore = 'https://developer.chrome.com/docs/webstore';
+  const youtube = 'https://youtube.com';
+  const otherYoutube = 'https://www.youtube.com';
 
   chrome.action.onClicked.addListener(async (tab) => {
-    if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+    if (tab.url.startsWith(youtube) || tab.url.startsWith(otherYoutube)) {
       // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
       const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
       // Next state will always be the opposite
